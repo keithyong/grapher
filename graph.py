9dbc56feb8448dc9af4a6fbef2ccc13841e37937
Keith Yong <terda12@gmail.com>
2014-10-11 19:42:21 -0400

    Added rudimentary testing input functionality

--- graph.py
+++ graph.py
@@ -15,7 +15,7 @@ graph_line_color = (100, 100, 100)
 
 size = [500, 500]
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Grapher by Keith Yong")
+pygame.display.set_caption("Grapher - Simple Graph Theory by Keith Yong")
 
 done = False
 clock = pygame.time.Clock()
@@ -31,8 +31,13 @@ for i in  drange(0, SCREEN_X, ZOOM):
 
 for i in drange(0, SCREEN_Y, ZOOM):
     pygame.draw.aaline(screen, graph_line_color, [0, i], [SCREEN_X, i], True)
+
 pygame.display.flip()
 
 while not done:
+    vertex_x = ZOOM * int(input("What is x: "))
+    vertex_y = ZOOM * int(input("What is y: "))
+    pygame.draw.circle(screen, RED, (vertex_x, vertex_y), 5)
+    pygame.display.flip()
     clock.tick(10)
 
